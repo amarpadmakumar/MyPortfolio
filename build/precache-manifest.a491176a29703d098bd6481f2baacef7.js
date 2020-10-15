8a2de640fcb12a5ce06bbb5f1ee37bf7d750081c
amarpadmakumar <padmakua@uci.edu>
2020-10-14 18:48:11 -0700

    first

---
+++ build/precache-manifest.a491176a29703d098bd6481f2baacef7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc9b4483eb0349effaa43b8fa3f3ff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21340fc2ec61e81eb826",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "010ace3734d9f9a391b8",
+    "url": "/static/css/main.255324e0.chunk.css"
+  },
+  {
+    "revision": "21340fc2ec61e81eb826",
+    "url": "/static/js/2.ff583251.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ff583251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "010ace3734d9f9a391b8",
+    "url": "/static/js/main.2bf9fec3.chunk.js"
+  },
+  {
+    "revision": "d08b0bd1c72fe20bc126",
+    "url": "/static/js/runtime-main.d4fcb62d.js"
+  },
+  {
+    "revision": "513bc5324a77acdce4277e919845f34c",
+    "url": "/static/media/amar.513bc532.jpeg"
+  },
+  {
+    "revision": "2cbc2a0494e289338814420c6c0b5c43",
+    "url": "/static/media/pool.2cbc2a04.gif"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MyPortfolio/precache-manifest.a68f1a292369c3518b5bfc8e61dd4f13.js"
+  "/precache-manifest.a491176a29703d098bd6481f2baacef7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
